de74034c55881145767bca9eaa431d5a1674cd36
Qu33nB <bdcotton52@gmail.com>
2020-05-21 13:09:09 -0500

    Edited createsuperuser command

--- custom_user/models.py
+++ custom_user/models.py
@@ -7,3 +7,5 @@ class CustomUser(AbstractUser):
     display_name = models.CharField(max_length=50, null=True, blank=True)
     homepage = models.URLField(max_length=200, null=True, blank=True)
     age = models.IntegerField(null=True, blank=True)
+
+    REQUIRED_FIELDS = ['display_name', 'homepage', 'age']
